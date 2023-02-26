8e3e371990a667fd8445ed11e4d49fc567a39a69
Mikhail Leonov <mikhail.leonov62@gmail.com>
2023-02-26 17:25:03 +0300

    App now can start

--- src/main/kotlin/team/microchad/config/JiraConfiguration.kt
+++ src/main/kotlin/team/microchad/config/JiraConfiguration.kt
@@ -4,10 +4,12 @@ import team.microchad.plugins.Secrets
 
 class JiraConfiguration {
     //TODO Get this values from application.yaml or PATH variables
-    val baseUrl = "tin-workshop.ddns.net:8080"  //TODO Use localhost:$PORT instead hardcoded URL, maybe separate test and prod parameters
+    val baseUrl =
+        "tin-workshop.ddns.net:8080"  //TODO Use localhost:$PORT instead hardcoded URL, maybe separate test and prod parameters
     val apiPath = "rest/api/2"
     val searchJql = "search/jql"
     val issue = "issue"
     val comment = "comment"
     val botUsername = Secrets.botUsername
     val botPassword = Secrets.botPassword
+}
