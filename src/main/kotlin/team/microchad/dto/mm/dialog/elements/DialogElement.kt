dbbc549db02fee8cd3f4766c6af7c7e3bef87116
Makarov Oleg <bogdanolegom@gmail.com>
2023-03-11 18:44:45 +0300

    Add selection by fields into getIssuesByProject method.

--- src/main/kotlin/team/microchad/dto/mm/dialog/elements/DialogElement.kt
+++ src/main/kotlin/team/microchad/dto/mm/dialog/elements/DialogElement.kt
@@ -8,7 +8,7 @@ import kotlinx.serialization.Serializable
  */
 
 
-///TODO: Bring the fields of all child classes in accordance with the conventions for the code.
+//TODO: Bring the fields of all child classes in accordance with the conventions for the code.
 //TODO: Pass all child classes fields to constructors according to Kotlin features (Default Constructor Options)
 @Serializable
 sealed class DialogElement {
