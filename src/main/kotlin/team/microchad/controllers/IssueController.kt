a8aa1d4079e9b6c6a2cafdd9f91d3483a4a91e53
Mikhail Leonov <mikhail.leonov62@gmail.com>
2023-03-11 19:37:29 +0300

    Try to add comment issue

--- src/main/kotlin/team/microchad/controllers/IssueController.kt
+++ src/main/kotlin/team/microchad/controllers/IssueController.kt
@@ -1,4 +1,31 @@
 package team.microchad.controllers
 
-class IssueController {
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import team.microchad.client.JiraClient
+import team.microchad.client.MmClient
+import team.microchad.dto.mm.IncomingMsg
+import team.microchad.dto.mm.dialog.Response
+import team.microchad.dto.mm.dialog.submissions.StatisticsSubmission
+import team.microchad.dto.mm.slash.ActionResponse
+import team.microchad.service.UserService
+import team.microchad.service.createChooseProjectDialog
+import team.microchad.service.getIssuesByProject
+
+class IssueController: KoinComponent {
+    private val mmClient: MmClient by inject()
+    private val jiraClient: JiraClient by inject()
+    val userService: UserService by inject()
+
+    suspend fun openProjectDialog(incoming: IncomingMsg): ActionResponse {
+        val projects = jiraClient.getProjects()
+        val dialog = createChooseProjectDialog(incoming.triggerId, projects)
+        mmClient.openDialog(dialog)
+        return ActionResponse("Comment in dialog window")
+    }
+
+    suspend fun createIssue(incoming: Response<StatisticsSubmission>) {
+
+    }
+
 }
