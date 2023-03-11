fb95adcd40fec589ef59028a4b4d465b127731e7
Mikhail Leonov <mikhail.leonov62@gmail.com>
2023-03-11 11:31:19 +0300

    Some refactor JiraClient.kt: Deleted unused imports. Requests to jira now are methods: without encodedParams and with encodedParams.

--- src/main/kotlin/team/microchad/client/JiraClient.kt
+++ src/main/kotlin/team/microchad/client/JiraClient.kt
@@ -10,12 +10,9 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
-
 import kotlinx.serialization.json.Json
-
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-
 import team.microchad.config.JiraConfiguration
 import team.microchad.dto.jira.*
 import team.microchad.exceptions.JiraBadRequestException
@@ -24,52 +21,16 @@ import team.microchad.exceptions.JiraBadRequestException
 class JiraClient : KoinComponent {
 
     private val configuration: JiraConfiguration by inject()
-    private val client = HttpClient(Java) {
-        install(Auth) {
-            basic {
-                sendWithoutRequest { true }
-                credentials {
-                    BasicAuthCredentials(configuration.botUsername, configuration.botPassword)
-                }
-            }
-        }
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient = true
-                ignoreUnknownKeys = true
-            })
-        }
-    }
+
 
     //TODO method to just send a jql. Create a jqlFactory
     suspend fun getByJql(jql: String): JiraJqlResponse {
-        val response: HttpResponse = client.get {
-            url {
-                protocol = URLProtocol.HTTP
-                host = configuration.baseUrl
-                appendPathSegments(configuration.apiPath, configuration.searchJql)
-                encodedParameters.append("jql", jql)
-                trailingQuery = true
-            }
+        val params = Parameters.build {
+            append("jql", jql)
         }
+        val response = httpResponseByJiraWithEncodedParams(configuration.searchJql, params)
         if (response.status == HttpStatusCode.OK)
             return response.body()
-        else
-            throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
-    }
-    suspend fun getByJqlStr(jql: String): String {
-        val response: HttpResponse = client.get {
-            url {
-                protocol = URLProtocol.HTTP
-                host = configuration.baseUrl
-                appendPathSegments(configuration.apiPath, configuration.searchJql)
-                encodedParameters.append("jql", jql)
-                trailingQuery = true
-            }
-        }
-        if (response.status == HttpStatusCode.OK)
-            return response.bodyAsText()
         else
             throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
     }
@@ -79,7 +40,7 @@ class JiraClient : KoinComponent {
             url {
                 protocol = URLProtocol.HTTP
                 host = configuration.baseUrl
-                with(configuration){
+                with(configuration) {
                     appendPathSegments(apiPath, issue)
                 }
                 setBody(Issue)
@@ -104,61 +65,6 @@ class JiraClient : KoinComponent {
         return response.status == HttpStatusCode.NoContent
     }
 
-    suspend fun getStatuses(): Array<Status> {
-        val response: HttpResponse = client.get {
-            url {
-                protocol = URLProtocol.HTTP
-                host = configuration.baseUrl
-                appendPathSegments(configuration.apiPath, configuration.status)
-                trailingQuery = true
-            }
-            headers {
-                contentType(ContentType.Application.Json)
-            }
-        }
-        if (response.status == HttpStatusCode.OK)
-            return response.body()
-        else
-            throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
-    }
-
-    suspend fun getProjects(): Array<Project> {
-        val response: HttpResponse = client.get {
-            url {
-                protocol = URLProtocol.HTTP
-                host = configuration.baseUrl
-                appendPathSegments(configuration.apiPath, configuration.project)
-                trailingQuery = true
-            }
-            headers {
-                contentType(ContentType.Application.Json)
-            }
-        }
-        if (response.status == HttpStatusCode.OK)
-            return response.body()
-        else
-            throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
-    }
-
-    suspend fun getUsers(): Array<User> {
-        val response: HttpResponse = client.get {
-            url {
-                protocol = URLProtocol.HTTP
-                host = configuration.baseUrl
-                appendPathSegments(configuration.apiPath, "user/search")
-                encodedParameters.append("username", ".")
-                trailingQuery = true
-            }
-            headers {
-                contentType(ContentType.Application.Json)
-            }
-        }
-        if (response.status == HttpStatusCode.OK)
-            return response.body()
-        else
-            throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
-    }
-
     suspend fun commentIssue(issueKey: String, newComment: String): Boolean {
         val response: HttpResponse = client.post {
             url {
@@ -177,6 +83,76 @@ class JiraClient : KoinComponent {
             }
         }
         return response.status == HttpStatusCode.Created
+    }
+
+    suspend fun getStatuses(): Array<Status> {
+        val response = httpResponseByJira(configuration.status)
+        if (response.status == HttpStatusCode.OK)
+            return response.body()
+        else
+            throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
+    }
+
+    suspend fun getProjects(): Array<Project> {
+        val response = httpResponseByJira(configuration.project)
+        if (response.status == HttpStatusCode.OK)
+            return response.body()
+        else
+            throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
+    }
+
+    suspend fun getUsers(): Array<User> {
+        val params = Parameters.build {
+            append("username", ".")
+        }
+        val response = httpResponseByJiraWithEncodedParams("user/search", params)
+        if (response.status == HttpStatusCode.OK)
+            return response.body()
+        else
+            throw JiraBadRequestException("Jira return ${response.status}. Check if the request is correct.")
+    }
+
+    private val client = HttpClient(Java) {
+        install(Auth) {
+            basic {
+                sendWithoutRequest { true }
+                credentials {
+                    BasicAuthCredentials(configuration.botUsername, configuration.botPassword)
+                }
+            }
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    private suspend fun httpResponseByJira(endpoint: String): HttpResponse = client.get {
+        url {
+            protocol = URLProtocol.HTTP
+            host = configuration.baseUrl
+            appendPathSegments(configuration.apiPath, endpoint)
+            trailingQuery = true
+        }
+        headers {
+            contentType(ContentType.Application.Json)
+        }
+    }
+
+    private suspend fun httpResponseByJiraWithEncodedParams(endpoint: String, params: Parameters) = client.get {
+        url {
+            protocol = URLProtocol.HTTP
+            host = configuration.baseUrl
+            appendPathSegments(configuration.apiPath, endpoint)
+            encodedParameters.appendAll(params)
+            trailingQuery = true
+        }
+        headers {
+            contentType(ContentType.Application.Json)
+        }
     }
 
 }
