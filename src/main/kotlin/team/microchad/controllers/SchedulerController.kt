33aa735ab1a292efd156eff1f24e80efe88896f3
Mikhail Leonov <mikhail.leonov62@gmail.com>
2023-03-12 12:00:44 +0300

    Some refactoring: Strings to const in SchedulerController.kt.

--- src/main/kotlin/team/microchad/controllers/SchedulerController.kt
+++ src/main/kotlin/team/microchad/controllers/SchedulerController.kt
@@ -2,6 +2,7 @@ package team.microchad.controllers
 
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+
 import team.microchad.client.JiraClient
 import team.microchad.client.MmClient
 import team.microchad.dto.mm.IncomingMsg
@@ -11,6 +12,16 @@ import team.microchad.dto.mm.dialog.submissions.SchedulerSubmission
 import team.microchad.dto.mm.slash.ActionResponse
 import team.microchad.model.repositories.ProjectMapRepository
 import team.microchad.service.createSchedulerDialog
+
+private const val CONTINUE_SETTING_IN_DIALOG_WINDOW = "Continue setting in dialog window"
+
+private const val SCHEDULER_SETTINGS_FAILED = "Scheduler setting is failed! You need to bind project first!"
+
+private const val ERROR = "Error"
+
+private const val BIND_PROJECT_ERROR = "Error! You need bind project to chat first!"
+
+private const val SETTINGS_CHANGED = "Settings changed"
 
 class SchedulerController : KoinComponent {
 
@@ -25,38 +36,37 @@ class SchedulerController : KoinComponent {
         return if (entity != null) {
             val dialog = createSchedulerDialog(incomingMsg.triggerId, projects, entity)
             mmClient.openDialog(dialog)
-            ActionResponse("Continue setting in dialog window")
+            ActionResponse(CONTINUE_SETTING_IN_DIALOG_WINDOW)
         } else {
             val chat = mmClient.createDirectChannel(incomingMsg.userId)
-            mmClient.sendToDirectChannel(OutgoingMsg(chat, "Scheduler setting is failed!" +
-                    " You need to bind project first!"))
-            ActionResponse("Error")
+            val message = OutgoingMsg(chat, SCHEDULER_SETTINGS_FAILED)
+            mmClient.sendToDirectChannel(message)
+            ActionResponse(ERROR)
         }
 
     }
 
-    suspend fun configureScheduler(incoming: Response<SchedulerSubmission>) =
-        with(incoming) {
-            val monday = submission?.mondayRadio
-            val friday = submission?.fridayRadio
-            val daily = submission?.dailyRadio
+    suspend fun configureScheduler(incoming: Response<SchedulerSubmission>) = with(incoming) {
+        val monday = submission?.mondayRadio
+        val friday = submission?.fridayRadio
+        val daily = submission?.dailyRadio
 
-            var message = "Settings changed"
-            val entity = projectMapRepository.findById(channelId)?.let {
-                it.friday = friday == "on"
-                it.monday = monday == "on"
-                it.everyday = daily == "on"
-                it
-            }
-            if (entity == null) {
-                message = "Error! You need bind project to chat first!"
-            } else {
-                projectMapRepository.update(entity.chat, entity)
-            }
-
-            val chatId = mmClient.createDirectChannel(incoming.userId)
-            val outgoingMessage = OutgoingMsg(chatId, message)
-            mmClient.sendToDirectChannel(outgoingMessage)
+        val entity = projectMapRepository.findById(channelId)?.let {
+            it.friday = friday == "on"
+            it.monday = monday == "on"
+            it.everyday = daily == "on"
+            it
         }
+        val message = if (entity == null) {
+            BIND_PROJECT_ERROR
+        } else {
+            projectMapRepository.update(entity.chat, entity)
+            SETTINGS_CHANGED
+        }
+
+        val chatId = mmClient.createDirectChannel(incoming.userId)
+        val outgoingMessage = OutgoingMsg(chatId, message)
+        mmClient.sendToDirectChannel(outgoingMessage)
+    }
 
 }
